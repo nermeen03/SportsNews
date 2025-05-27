3137cadde2c2aa1ddd5d8751281a4e8ecb9a4294
Mario <90201555+MarioAbdelMaseeh@users.noreply.github.com>
2025-05-27 16:00:44 +0300

    initial commit

---
+++ SportsNews/HomeScreen/View/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportsNews
+//
+//  Created by mac on 27/05/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
