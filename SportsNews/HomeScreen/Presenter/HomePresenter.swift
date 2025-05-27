d7fb77964d1300a3da045c0a924fceffcb37d5a6
Mario <90201555+MarioAbdelMaseeh@users.noreply.github.com>
2025-05-27 16:35:43 +0300

    creating presenters empty files

---
+++ SportsNews/HomeScreen/Presenter/HomePresenter.swift
@@ -0,0 +1,7 @@
+//
+//  HomePresenter.swift
+//  SportsNews
+//
+//  Created by mac on 27/05/2025.
+//
+
---
+++ SportsNews/Models/RemoteServices/Network.swift
@@ -0,0 +1,7 @@
+//
+//  Network.swift
+//  SportsNews
+//
+//  Created by mac on 27/05/2025.
+//
+
