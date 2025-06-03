543392430a888ea038c2f4317ccd3b9ecdd82fee
Nermeen Mohamed <nermeen41292@gmail.com>
2025-06-03 13:32:11 +0300

    Creating localization files

---
+++ SportsNews/Common/Functions/Localization.swift
@@ -0,0 +1,15 @@
+//
+//  Localization.swift
+//  SportsNews
+//
+//  Created by Nermeen Mohamed on 03/06/2025.
+//
+
+import Foundation
+import UIKit
+
+extension String {
+    var localized: String {
+        return NSLocalizedString(self, comment: "")
+    }
+}
